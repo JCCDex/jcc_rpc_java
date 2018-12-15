782d2616ab7045e4020d47a02982d92a15c203c2
GinMu <gin.musheng@gmail.com>
2018-12-15 10:06:55 +0800

    Update JConstant.java

--- rpc/src/main/java/com/jccdex/rpc/config/JConstant.java
+++ rpc/src/main/java/com/jccdex/rpc/config/JConstant.java
@@ -5,20 +5,20 @@ public class JConstant {
 	// jingchang api success code
 	public static final String REQUEST_JC_SUCCESS_CODE = "0";
 
-	// jingchang api url
-	public static final String JC_REQUEST_BALANCE_URL = "/exchange/balances/";
+	// jingchang api route
+	public static final String JC_REQUEST_BALANCE_ROUTE = "/exchange/balances/";
 
-	public static final String JC_REQUEST_HISTORY_URL = "/exchange/tx/";
+	public static final String JC_REQUEST_HISTORY_ROUTE = "/exchange/tx/";
 
-	public static final String JC_REQUEST_ORDER_URL = "/exchange/orders/";
+	public static final String JC_REQUEST_ORDER_ROUTE = "/exchange/orders/";
 
-	public static final String JC_CREATE_ORDER_URL = "/exchange/sign_order";
+	public static final String JC_CREATE_ORDER_ROUTE = "/exchange/sign_order";
 
-	public static final String JC_CANCEL_ORDER_URL = "/exchange/sign_cancel_order";
+	public static final String JC_CANCEL_ORDER_ROUTE = "/exchange/sign_cancel_order";
 
-	public static final String JC_REQUEST_SEQUENCE = "/exchange/sequence/";
+	public static final String JC_REQUEST_SEQUENCE_ROUTE = "/exchange/sequence/";
 
-	public static final String JC_TRNSFER_TOKEN_URL = "/exchange/sign_payment";
+	public static final String JC_TRNSFER_TOKEN_ROUTE = "/exchange/sign_payment";
 
 	/**
 	 * return -1 if request sequence in error
